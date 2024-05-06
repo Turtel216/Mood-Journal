9b9a9588884a1881b6e5e35c803afa45db608681
Turtel216 <papadimitrios216@gmail.com>
2024-05-06 18:18:35 +0300

    added server folder

---
+++ Server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.dimitrios_papakonstantinou.mood_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
