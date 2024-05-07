0dd201154cc628d6356dc3e55dba456910842257
Turtel216 <papadimitrios216@gmail.com>
2024-05-07 23:24:13 +0300

    Added Entry Repository

---
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/repositories/EntryRepository.java
@@ -0,0 +1,7 @@
+package com.dimitrios_papakonstantinou.mood_journal.datasource.repositories;
+
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntryRepository extends JpaRepository<Entry, Long> {
+}
