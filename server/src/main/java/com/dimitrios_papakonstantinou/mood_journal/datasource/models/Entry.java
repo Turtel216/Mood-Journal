2d9238c8a5efa512bf1f728253d92952241be50d
Turtel216 <papadimitrios216@gmail.com>
2024-05-07 23:19:55 +0300

    Added Entry model

---
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/models/Entry.java
@@ -0,0 +1,23 @@
+package com.dimitrios_papakonstantinou.mood_journal.datasource.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "entry")
+@Getter
+@Setter
+public class Entry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Mood mood;
+    @Column(nullable = false)
+    //Todo refactor to add actual date
+    private String date;
+}
