2f115fdfdef2a3654ced8e67c8e69629495b2560
Turtel216 <papadimitrios216@gmail.com>
2024-05-08 22:29:45 +0300

    Update WebApp controller

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppController.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppController.java
@@ -1,10 +1,17 @@
 package com.dimitrios_papakonstantinou.mood_journal.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+import com.dimitrios_papakonstantinou.mood_journal.service.implementation.WebAppServiceImplementation;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/moodjournal")
 public class WebAppController {
+
+    private WebAppServiceImplementation webAppService;
+
+    @PostMapping("/entry")
+    public String addEntry(@RequestBody Entry entry) {
+        return webAppService.saveEntry(entry);
+    }
 }
