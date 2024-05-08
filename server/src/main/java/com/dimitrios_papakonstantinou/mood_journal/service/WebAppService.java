26e8fa7bbfaee762b0c56113482d3bd14e6f8002
Turtel216 <papadimitrios216@gmail.com>
2024-05-08 22:27:06 +0300

    Updated WebApp service layer

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/WebAppService.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/WebAppService.java
@@ -1,5 +1,8 @@
 package com.dimitrios_papakonstantinou.mood_journal.service;
 
 
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+
 public interface WebAppService {
+    public String addEntry(Entry entry);
 }
--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/implementation/WebAppServiceImplementation.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/implementation/WebAppServiceImplementation.java
@@ -1,7 +1,14 @@
 package com.dimitrios_papakonstantinou.mood_journal.service.implementation;
 
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+import com.dimitrios_papakonstantinou.mood_journal.service.WebAppService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class WebAppServiceImplementation {
+public class WebAppServiceImplementation implements WebAppService {
+
+    public String addEntry(Entry entry) {
+        //TODO implements method
+        return "Succes";
+    }
 }
