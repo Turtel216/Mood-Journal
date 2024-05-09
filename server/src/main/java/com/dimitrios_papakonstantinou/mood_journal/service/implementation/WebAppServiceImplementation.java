fb038487b3e0780c5db6ddbcc9943a39efde23a3
Turtel216 <papadimitrios216@gmail.com>
2024-05-09 23:16:50 +0300

    Updated WebApp service layer

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/implementation/WebAppServiceImplementation.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/implementation/WebAppServiceImplementation.java
@@ -1,14 +1,29 @@
 package com.dimitrios_papakonstantinou.mood_journal.service.implementation;
 
 import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+import com.dimitrios_papakonstantinou.mood_journal.datasource.repositories.EntryRepository;
+import com.dimitrios_papakonstantinou.mood_journal.datasource.repositories.UserRepository;
 import com.dimitrios_papakonstantinou.mood_journal.service.WebAppService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class WebAppServiceImplementation implements WebAppService {
 
+    private EntryRepository entryRepository;
+    private UserRepository userRepository;
+
     public String saveEntry(Entry entry) {
-        //TODO implements method
-        return "Success";
+        if(userRepository.findById(entry.getUserId()).isPresent()) {
+            try {
+                entryRepository.save(entry);
+                return "Success";
+            } catch(Exception e){
+                //TODO throw proper exception
+                return "Failure" + e.toString();
+            }
+        }
+        return "Invalid user ID";
     }
 }
