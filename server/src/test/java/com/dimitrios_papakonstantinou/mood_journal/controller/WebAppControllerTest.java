b5205e53bc4466a97a6d2835a886adb1d281fa6b
Turtel216 <papadimitrios216@gmail.com>
2024-05-10 22:11:16 +0300

    Added WebAppController test

---
+++ server/src/test/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppControllerTest.java
@@ -0,0 +1,16 @@
+package com.dimitrios_papakonstantinou.mood_journal.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebAppControllerTest {
+
+    @Test
+    void addEntry() {
+    }
+
+    @Test
+    void getEntry() {
+    }
+}
