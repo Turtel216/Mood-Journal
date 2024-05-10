626dbdf68c090ff39625d8a27be212276d5508a7
Turtel216 <papadimitrios216@gmail.com>
2024-05-10 22:05:23 +0300

    Removed EntryResponse

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/models/respnonses/EntryResponse.java
+++
@@ -1,6 +0,0 @@
-package com.dimitrios_papakonstantinou.mood_journal.datasource.models.respnonses;
-
-import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
-
-public class EntryResponse extends Entry {
-}
