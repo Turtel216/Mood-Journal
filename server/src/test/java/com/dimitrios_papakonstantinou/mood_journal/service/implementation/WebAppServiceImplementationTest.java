5a032c040433edf6843d6c707869aaaa92303080
Turtel216 <papadimitrios216@gmail.com>
2024-05-11 21:30:50 +0300

    Added WebAppServiceImplementationTest

---
+++ server/src/test/java/com/dimitrios_papakonstantinou/mood_journal/service/implementation/WebAppServiceImplementationTest.java
@@ -0,0 +1,26 @@
+package com.dimitrios_papakonstantinou.mood_journal.service.implementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebAppServiceImplementationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveEntry() {
+    }
+
+    @Test
+    void getEntry() {
+    }
+}
