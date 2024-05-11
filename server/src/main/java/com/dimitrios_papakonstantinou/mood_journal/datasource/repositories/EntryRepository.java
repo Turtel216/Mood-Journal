26df18e933c8065fbf01ecd072de40919307d435
Turtel216 <papadimitrios216@gmail.com>
2024-05-11 17:14:34 +0300

    Updated Entry repository interface

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/repositories/EntryRepository.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/repositories/EntryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EntryRepository extends JpaRepository<Entry, Long> {
-    //TODO add find by date
+    List<Entry> findByUserId(Long userId);
     //TODO add find by UserId
 }
