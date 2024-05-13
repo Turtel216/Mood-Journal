ad25cfb416b062ad3a2b3ad53cde9a0cd562cacb
turtel42 <papadimitrios216@gmail.com>
2024-05-14 00:32:48 +0300

    Added testGetEntry_Success

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppController.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppController.java
@@ -2,10 +2,10 @@ package com.dimitrios_papakonstantinou.mood_journal.controller;
 
 import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
 import com.dimitrios_papakonstantinou.mood_journal.response.ResponseHandler;
+import com.dimitrios_papakonstantinou.mood_journal.service.WebAppService;
 import com.dimitrios_papakonstantinou.mood_journal.service.implementation.WebAppServiceImplementation;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/moodjournal")
 @AllArgsConstructor
-@NoArgsConstructor
 public class WebAppController {
 
-    private WebAppServiceImplementation webAppService;
+    private WebAppService webAppService;
 
     @PostMapping("/entry")
     public String addEntry(@RequestBody Entry entry) {
@@ -25,7 +24,7 @@ public class WebAppController {
 
     // saveEntry throws exception on userId not found and returns an updated NotFound response to client
     @GetMapping("/entry/{userId}")
-    public ResponseEntity<Object> getEntry(@PathVariable Long userId) {
+    public ResponseEntity<Object> getEntry(@PathVariable("userId") Long userId) {
         var currentDate = "05.05.2024";
         var entry = webAppService.getEntry(userId, currentDate);
 
--- server/src/test/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppControllerTest.java
+++ server/src/test/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppControllerTest.java
@@ -1,16 +1,55 @@
 package com.dimitrios_papakonstantinou.mood_journal.controller;
 
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Mood;
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.User;
+import com.dimitrios_papakonstantinou.mood_journal.service.WebAppService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(WebAppController.class)
 class WebAppControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private WebAppService webAppService;
+    Entry entry;
+    User user;
+    Mood mood;
+
+    @BeforeEach
+    void setUp() {
+        mood = Mood.GOOD;
+        entry = new Entry(1L,1L, "some text", mood, "05.05.2024");
+        user = new User(1L, "Alex", "password", "alexis@mail.com");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 
     @Test
     void addEntry() {
     }
 
+    //Test case Success
     @Test
-    void getEntry() {
+    void testGetEntry_Success() throws Exception {
+        when(webAppService.getEntry(entry.getUserId(), entry.getEntryDate())).thenReturn(entry);
+
+        this.mockMvc.perform(get("/api/moodjournal/entry/1"))
+                .andDo(print()).andExpect(status().isOk());
     }
 }
