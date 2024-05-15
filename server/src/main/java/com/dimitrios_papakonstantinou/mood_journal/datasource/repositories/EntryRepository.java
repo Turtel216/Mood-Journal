d3cd5c15f676143b8c80862e1f6a3949bd9b4a9b
turtel42 <papadimitrios216@gmail.com>
2024-05-16 00:10:33 +0300

    Updated EntryRepository

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/repositories/EntryRepository.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/datasource/repositories/EntryRepository.java
@@ -3,8 +3,10 @@ package com.dimitrios_papakonstantinou.mood_journal.datasource.repositories;
 import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EntryRepository extends JpaRepository<Entry, Long> {
     Optional<Entry> findByEntryDateAndUserId(String date, Long userId);
+    Optional<List<Entry>> findByUserId(Long userId);
 }
