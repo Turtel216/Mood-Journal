9d21492bdc6c016e67b48ffe28d6987e32df577f
turtel42 <papadimitrios216@gmail.com>
2024-05-16 02:39:54 +0300

    Added EntryAnalyzer

--- server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppController.java
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//TODO Update Testing
+
 @RestController
 @RequestMapping("/api/moodjournal")
 @AllArgsConstructor
---
+++ server/src/main/java/com/dimitrios_papakonstantinou/mood_journal/service/util/EntryAnalyzer.java
@@ -0,0 +1,41 @@
+package com.dimitrios_papakonstantinou.mood_journal.service.util;
+
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
+import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Mood;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EntryAnalyzer {
+
+    public static Map<Mood, Integer> countMoods(List<Mood> moods) {
+
+        Map<Mood, Integer> counter = new HashMap<Mood, Integer>();
+
+        moods.forEach(mood -> {
+            switch (mood) {
+                case GREATE -> counter.put(Mood.GREATE, counter.get(Mood.GREATE) + 1);
+                case GOOD -> counter.put(Mood.GOOD, counter.get(Mood.GOOD) + 1);
+                case BAD -> counter.put(Mood.BAD, counter.get(Mood.BAD) + 1);
+                case HORRIBLE -> counter.put(Mood.BAD, counter.get(Mood.HORRIBLE) + 1);
+            }
+        });
+        return counter;
+    }
+
+    public static Mood averageMood(List<Entry> entries) {
+        //Convert List of entries to Array of Moods
+        var moods = entries.stream().map(Entry::getMood).toList();
+
+        // count of each mood
+        Map<Mood, Integer> moodCount = countMoods(moods);
+
+        // return the mood with the highest number of entries. Return null if there is no max
+        return moodCount.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+
+    }
+}
