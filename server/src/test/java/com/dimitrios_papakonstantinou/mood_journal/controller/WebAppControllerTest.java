1e1a22a61d07e32a2dc65af08f91405854210c53
Turtel216 <papadimitrios216@gmail.com>
2024-05-22 23:42:52 +0300

    Updated controller tests, tests build failing

--- server/src/test/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppControllerTest.java
+++ server/src/test/java/com/dimitrios_papakonstantinou/mood_journal/controller/WebAppControllerTest.java
@@ -3,7 +3,8 @@ package com.dimitrios_papakonstantinou.mood_journal.controller;
 import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Entry;
 import com.dimitrios_papakonstantinou.mood_journal.datasource.models.Mood;
 import com.dimitrios_papakonstantinou.mood_journal.datasource.models.User;
-import com.dimitrios_papakonstantinou.mood_journal.service.WebAppService;
+import com.dimitrios_papakonstantinou.mood_journal.exceptions.UserIdNotFoundException;
+import com.dimitrios_papakonstantinou.mood_journal.service.implementation.WebAppServiceImplementation;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,7 +26,7 @@ class WebAppControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private WebAppService webAppService;
+    private WebAppServiceImplementation webAppService;
     Entry entry;
     User user;
     Mood mood;
@@ -38,18 +40,36 @@ class WebAppControllerTest {
 
     @AfterEach
     void tearDown() {
+        mood = null;
+        entry = null;
+        user = null;
     }
 
     @Test
-    void addEntry() {
+    void addEntry() throws Exception {
+        when(webAppService.saveEntry(entry)).thenReturn("Entry saved successfully");
+
+        this.mockMvc.perform(post("/api/moodjournal/entry", entry))
+                .andDo(print()).andExpect(status().isOk());
     }
 
     //Test case Success
     @Test
     void testGetEntry_Success() throws Exception {
-        when(webAppService.getEntry(entry.getUserId(), entry.getEntryDate())).thenReturn(entry);
+        when(webAppService.getEntry(entry.getUserId(), entry.getEntryDate()))
+                .thenReturn(entry);
 
         this.mockMvc.perform(get("/api/moodjournal/entry/1"))
                 .andDo(print()).andExpect(status().isOk());
+    }
+
+    //Test case Failure
+    @Test
+    void testGetEntry_ThrowsException() throws Exception {
+        when(webAppService.getEntry(entry.getUserId(), entry.getEntryDate()))
+                .thenThrow(UserIdNotFoundException.class);
+
+        this.mockMvc.perform(get("/api/moodjournal/entry/1"))
+                .andDo(print()).andExpect(status().isNotFound());
     }
 }
